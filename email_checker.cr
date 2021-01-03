4f941c166a4ccf807b73853188a949425a668501
Damir Sharipov <dammer2k@gmail.com>
2021-01-03 15:08:25 +0300

    quiet mode

--- email_checker.cr
+++ email_checker.cr
@@ -7,56 +7,13 @@ email = ""
 domain = ""
 mx_server = ""
 mx_port = 25
+quiet = false
 
-OptionParser.parse do |parser|
-  parser.banner = "Email address checking tool."
-
-  parser.on "-v", "--version", "Show version" do
-    puts "version 0.1"
-    exit
+class Logger
+  def initialize(@quiet : Bool); end
+  def log(line)
+    puts line unless @quiet
   end
-  parser.on "-h", "--help", "Show help" do
-    puts parser
-    exit
-  end
-
-  parser.on "-s SENDER", "--sender=SENDER", "Sender email for checking. Default is <mailvalid@example.com>" do |sender|
-    server = get_domain(sender)
-    account = get_account(email)
-  end
-
-  parser.on "-e ADDR", "--email=ADDR", "Email address for check" do |addr|
-    puts "Checking address: <#{email = addr}>"
-    domain = get_domain(email)
-    puts "MX is: #{mx_server = get_mx(domain)}"
-    puts "Start checking."
-    request_mx(email, mx_server, mx_port, server, account)
-    puts "Done."
-    exit
-  end
-end
-
-def request_mx(email, mx_server, mx_port, server, account)
-  result = false
-  TCPSocket.open(mx_server, mx_port) do |client|
-    client << "\n"
-    puts response = client.gets
-    break unless response.to_s.starts_with?("220")
-    puts request = "HELO #{server}\n"
-    client << request
-    puts response = client.gets
-    break unless response.to_s.starts_with?("250")
-    puts request = "MAIL FROM: <#{account}@#{server}>\n"
-    client << request
-    puts response = client.gets
-    break unless response.to_s.starts_with?("250")
-    puts request = "RCPT TO: <#{email}>\n"
-    client << request
-    puts response = client.gets
-    result = response.to_s.starts_with?("250")
-    puts "Email <#{email}> is #{ result ? "" : "in" }valid."
-  end
-  result
 end
 
 def get_domain(email)
@@ -71,3 +28,62 @@ def get_mx(domain)
   resp = `dig mx #{domain} +short`
   resp.lines.first.split().last
 end
+
+OptionParser.parse do |parser|
+  parser.banner = "Email address checking tool."
+
+  parser.on "-v", "--version", "Show version" do
+    puts "version 0.1"
+    exit
+  end
+
+  parser.on "-h", "--help", "Show help" do
+    puts parser
+    exit
+  end
+
+  parser.on "-q", "--quiet", "Show only test email and result, one line" do
+    quiet = true
+  end
+
+  parser.on "-s SENDER", "--sender=SENDER", "Sender email for checking. Default is <mailvalid@example.com>" do |sender|
+    server = get_domain(sender)
+    account = get_account(email)
+  end
+
+  parser.on "-e ADDR", "--email=ADDR", "Email address for check" do |addr|
+    email = addr
+    domain = get_domain(email)
+    mx_server = get_mx(domain)
+  end
+end
+
+logger = Logger.new(quiet)
+logger.log "Checking address: <#{email}>"
+logger.log "MX is: #{mx_server}"
+logger.log "Start checking."
+result = false
+
+TCPSocket.open(mx_server, mx_port) do |client|
+  client << "\n"
+  logger.log response = client.gets
+  break unless response.to_s.starts_with?("220")
+  logger.log request = "HELO #{server}\n"
+  client << request
+  logger.log response = client.gets
+  break unless response.to_s.starts_with?("250")
+  logger.log request = "MAIL FROM: <#{account}@#{server}>\n"
+  client << request
+  logger.log response = client.gets
+  break unless response.to_s.starts_with?("250")
+  logger.log request = "RCPT TO: <#{email}>\n"
+  client << request
+  logger.log response = client.gets
+  result = response.to_s.starts_with?("250")
+  puts "Email <#{email}> is #{ result ? "" : "in" }valid."
+  result
+end
+
+logger.log "Done."
+exit
+
